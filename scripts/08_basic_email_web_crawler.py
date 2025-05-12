89a2b66f7de6f47415f28641bd3595fa90e13660
Brivno1234 <seredaivan2004@gmail.com>
2025-05-12 14:52:47 +0300

    new update

--- scripts/08_basic_email_web_crawler.py
+++ scripts/08_basic_email_web_crawler.py
@@ -1,21 +1,17 @@
+from bs4 import BeautifulSoup 
 import requests
-import re
 
-# get url
+
+
+
 url = input('Enter a URL (include `http://`): ')
 
-# connect to the url
-website = requests.get(url)
+response = requests.get(url)
+html = response.text
+soup = BeautifulSoup(html,"html.parser")
+print(html)
 
-# read html
-html = website.text
-
-# use re.findall to grab all the links
-links = re.findall('"((http|ftp)s?://.*?)"', html)
-emails = re.findall('([\w\.,]+@[\w\.,]+\.\w+)', html)
-
-
-# print the number of links in the list
-print("\nFound {} links".format(len(links)))
-for email in emails:
-    print(email)
+list = []
+for i in soup.find_all("a",href=True):
+    list.append(i)
+    print("leited kink: ", i)
